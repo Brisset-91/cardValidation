adfe03e089a490767d5fcc84530039908f9b5840
Brisset <corcinobrisset@gmail.com>
2021-10-21 17:22:11 -0500

    primer proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import validator from './validator.js';
 
+
+const enterButton = document.getElementsByClassName("enterButton")[0];
+
+enterButton.addEventListener('click', cardNumber)
+
+function cardNumber (){
+
+    let maskify = document.getElementById("valueInput").innerHTML = validator.maskify(document.getElementsByClassName("cardNumber")[0].value);
+
+    document.getElementById("number").style.display="none"
+    document.getElementById("valueInput").style.display="block"
+  
+    console.log(maskify)
+    return maskify
+    
+}
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,70 @@
+
+
 const validator = {
-  // ...
+  
+   isValid: (numberCard) => {
+    
+    let validCard = numberCard.split('').reverse()
+    let newValidCard = []
+   
+      for (let i = 0; i < validCard.length; i++) {
+        
+        let car = validCard[i]
+
+          if (i%2 != 0) {
+            let par = parseInt(car)*2
+            
+            if (par >=10) {
+              
+              let sumaMayor = par.toString().split('')
+              let final= parseInt(sumaMayor[0]) + parseInt(sumaMayor[1]) 
+              
+              newValidCard += final
+            } else{
+              let menorDiez = par
+              newValidCard += menorDiez
+            }
+            
+        } else {
+            let inpar = parseInt(car)*1
+            newValidCard += inpar
+        }
+        
+      }
+
+      console.log(newValidCard)  
+
+   let validCardArray = newValidCard.split('')
+   console.log(validCardArray)
+
+    let sum = validCardArray.reduce((a,b)=>{
+  
+      return parseInt(a) + parseInt(b)
+
+    },[0])
+
+    let valTrue
+
+    sum%10==0? valTrue = true: valTrue = false;
+      
+    console.log(sum)
+
+    return valTrue;
+
+  },
+
+  
+  maskify: (newArray)=>{
+
+    let encryptedCard = newArray.slice(0,-4).replace(/[0-9,a-z]/g,"#")
+ 
+    let lastDigits =  newArray.slice(-4)
+   
+    let maski = encryptedCard.concat(lastDigits)
+  
+     return maski
+  }
+  
 };
 
 export default validator;
